f34f478aa089596678dcd41a7418c9d5e91d77f1
Mohammad Abbaspour <mhagalaxy@gmail.com>
2020-10-11 21:20:53 +0330

    server completed

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,16 +1,61 @@
+// require('dotenv').config()
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+// const {authMiddleware} = require('./utils/auth');
+//
+// const cors = require('cors');
+//
+// const { ApolloServer } = require('apollo-server-express')
+//
+// const {typeDefs, resolvers} = require('./schemas');
+//
+//
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+//
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
+//
+//
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+//
+// app.use(authMiddleware);
+//
+// // app.use(routes);
+// // initialize apollo server
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: ({req}) => {
+//         return {
+//             getUser: () => req.user
+//         }
+//     }
+// });
+// server.applyMiddleware({app});
+//
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
+
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const {authMiddleware} = require('./utils/auth');
-
 const cors = require('cors');
 
 const { ApolloServer } = require('apollo-server-express')
 
 const {typeDefs, resolvers} = require('./schemas');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,16 +64,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    console.log('here')
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(authMiddleware);
-
 // app.use(routes);
-// initialize apollo server
+
+app.use(authMiddleware);
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -41,5 +85,5 @@ const server = new ApolloServer({
 server.applyMiddleware({app});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
